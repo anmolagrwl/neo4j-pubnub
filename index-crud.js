97fd2e2a0ea9e753919c926f5eb75efaafd6a4c0
anmolonruby <anmol1771@gmail.com>
2015-08-02 18:07:33 +0530

    final version

---
+++ index-crud.js
@@ -0,0 +1,141 @@
+var db = require("seraph")
+
+var pubnub = require("pubnub")({
+    subscribe_key: 'demo', // always required
+    publish_key: 'demo'    // only required if publishing
+});
+
+var mumbai, delhi, train;
+
+// Creating a node with a label
+// --------------------------------
+// function create_node() {
+//   db.save({ name: 'Delhi' }, 'City', function(err, node) {
+//       if (err) throw err;
+//       console.log(node.name);
+//       delhi = (node.name);
+//   });
+//
+//   db.save({ name: 'Mumbai' }, 'City', function(err, node) {
+//       if (err) throw err;
+//       console.log(node.name);
+//       mumbai = (node.name);
+//   });
+//
+//   db.save({ name: 'train-1', 'available-seats': 50 }, 'Train', function(err, node) {
+//       if (err) throw err;
+//       console.log(node.name);
+//       train = (node.name);
+//   });
+// }
+//
+// create_node();
+
+// Finding nodes
+// ----------------
+// function find_node() {
+//
+//   mum = { name: "Mumbai" };
+//
+//   db.find(mum, function (err, result) {
+//       if (err) throw err;
+//       // console.log(result);
+//       for (r of result) {
+//         console.log(r);
+//         mumbai = r;
+//       }
+//   });
+//
+//   del = { name: "Delhi" };
+//
+//   db.find(del, function (err, result) {
+//       if (err) throw err;
+//       // console.log(result);
+//       for (r of result) {
+//         console.log(r);
+//         delhi = r;
+//       }
+//   });
+//
+//   tra = { name: "train-1" };
+//
+//   db.find(tra, function (err, result) {
+//       if (err) throw err;
+//       // console.log(result);
+//       for (r of result) {
+//         console.log(r);
+//         train = r;
+//       }
+//   });
+// }
+//
+// find_node();
+
+// Create relationships
+// -------------------------
+// mumbai = { name: 'Mumbai', id: 2 }
+// delhi = { name: 'Delhi', id: 1 }
+// train = { name: 'train-1', 'available-seats': 50, id: 0 }
+//
+// function create_rel() {
+//   db.relate(mumbai, 'origin', train, function(err, relationship) {
+//     console.log(relationship);
+//   });
+//
+//   db.relate(train, 'destination', delhi, function(err, relationship) {
+//     console.log(relationship);
+//   });
+// }
+//
+// create_rel();
+
+// Update node
+// ----------------------
+tra = { name: "train-1" };
+
+function update_node() {
+    db.find(tra, function (err, result) {
+        if (err) throw err;
+        // console.log(result);
+        for (r of result) {
+            // console.log(r);
+            train = r;
+        }
+        console.log(train);
+        train["available-seats"]--;
+        db.save(train, function(err, node) {
+            // console.log(node);
+            pubnub.publish({
+                channel: 'neo4j',
+                message: {"seats_left": node["available-seats"]},
+                callback: function(m){console.log(m)}
+            });
+            return node["available-seats"];
+        })
+    });
+}
+
+update_node();
+//
+// function find_seats() {
+//   db.find(tra, function (err, result) {
+//       if (err) throw err;
+//       var train;
+//       // console.log(result);
+//       for (r of result) {
+//         // console.log(r);
+//         train = r;
+//         console.log(train["available-seats"]);
+//         return train["available-seats"];
+//       }
+//       // console.log(train);
+//   });
+// }
+
+// find_seats();
+
+
+//START n = node(*)
+//OPTIONAL MATCH n-[r]-()
+//WHERE (ID(n)>0 AND ID(n)<10000)
+//DELETE n, r;
--- index.js
+++ index.js
@@ -1,135 +1,41 @@
-var db = require("seraph")
+var db = require("seraph")("http://localhost:7474");
 
 var pubnub = require("pubnub")({
     subscribe_key: 'demo', // always required
     publish_key: 'demo'    // only required if publishing
 });
 
-var mumbai, delhi, train;
-
-// Creating a node with a label
-// --------------------------------
-// function create_node() {
-//   db.save({ name: 'Delhi' }, 'City', function(err, node) {
-//       if (err) throw err;
-//       console.log(node.name);
-//       delhi = (node.name);
-//   });
-//
-//   db.save({ name: 'Mumbai' }, 'City', function(err, node) {
-//       if (err) throw err;
-//       console.log(node.name);
-//       mumbai = (node.name);
-//   });
-//
-//   db.save({ name: 'train-1', 'available-seats': 50 }, 'Train', function(err, node) {
-//       if (err) throw err;
-//       console.log(node.name);
-//       train = (node.name);
-//   });
-// }
-//
-// create_node();
-
-// Finding nodes
-// ----------------
-// function find_node() {
-//
-//   mum = { name: "Mumbai" };
-//
-//   db.find(mum, function (err, result) {
-//       if (err) throw err;
-//       // console.log(result);
-//       for (r of result) {
-//         console.log(r);
-//         mumbai = r;
-//       }
-//   });
-//
-//   del = { name: "Delhi" };
-//
-//   db.find(del, function (err, result) {
-//       if (err) throw err;
-//       // console.log(result);
-//       for (r of result) {
-//         console.log(r);
-//         delhi = r;
-//       }
-//   });
-//
-//   tra = { name: "train-1" };
-//
-//   db.find(tra, function (err, result) {
-//       if (err) throw err;
-//       // console.log(result);
-//       for (r of result) {
-//         console.log(r);
-//         train = r;
-//       }
-//   });
-// }
-//
-// find_node();
-
-// Create relationships
-// -------------------------
-// mumbai = { name: 'Mumbai', id: 2 }
-// delhi = { name: 'Delhi', id: 1 }
-// train = { name: 'train-1', 'available-seats': 50, id: 0 }
-//
-// function create_rel() {
-//   db.relate(mumbai, 'origin', train, function(err, relationship) {
-//     console.log(relationship);
-//   });
-//
-//   db.relate(train, 'destination', delhi, function(err, relationship) {
-//     console.log(relationship);
-//   });
-// }
-//
-// create_rel();
+var train;
 
 // Update node
 // ----------------------
-tra = { name: "train-1" };
+tra = { name: "Train-1" };
 
 function update_node() {
   db.find(tra, function (err, result) {
       if (err) throw err;
       // console.log(result);
       for (r of result) {
-        // console.log(r);
         train = r;
       }
-      console.log(train);
-      train["available-seats"]--;
+
+      train["availableSeats"]--; //reduces number of seats by 1
       db.save(train, function(err, node) {
-        // console.log(node);
-        pubnub.publish({
+        pubnub.publish({  //publishing the updated seat numbers through PubNub, in 'neo4j' channel
           channel: 'neo4j',
-          message: {"seats_left": node["available-seats"]},
+          message: {"seats_left": node["availableSeats"]},
           callback: function(m){console.log(m)}
         });
-        return node["available-seats"];
+        //return node["availableSeats"];
       })
+
+      console.log(train);
   });
 }
 
 update_node();
-//
-// function find_seats() {
-//   db.find(tra, function (err, result) {
-//       if (err) throw err;
-//       var train;
-//       // console.log(result);
-//       for (r of result) {
-//         // console.log(r);
-//         train = r;
-//         console.log(train["available-seats"]);
-//         return train["available-seats"];
-//       }
-//       // console.log(train);
-//   });
-// }
 
-// find_seats();
+
+//CREATE(:Train{name: "Train-1", availableSeats: 15})
+//CREATE(:City{name: "Mumbai"})
+//CREATE(:City{name: "Delhi"})
